7bc3da9ec237a8c2514be31fd0f0554cc8a1d793
BearWulfx <torushojiro@gmail.com>
2021-09-03 21:10:26 +0300

    changed to production and added allowed hosts

--- Nonogram_web/nonogram/settings.py
+++ Nonogram_web/nonogram/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t_s(ths!4_oy7+@_+gs(!9bh3sc3n$l5dmphzceb(n3u%ic!)'
+SECRET_KEY = 'dfkjsdflsdkhrfrtgmenj4564jl43n5jnrajxzdflkgjhrtgfd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "bearwulf.pythonanywhere.com" ]
 
 
 # Application definition
