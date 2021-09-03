acf8060fda6d22e8bd3460a6d79a99e0c088e6eb
BearWulfx <torushojiro@gmail.com>
2021-09-03 21:43:28 +0300

    set static root

--- Nonogram_web/nonogram/settings.py
+++ Nonogram_web/nonogram/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATIC_ROOT = "/var/www/static"
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
